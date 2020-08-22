bd695adbce5f039af09cf28352f62dbfada21ba9
Dakota <dakota.talley@gmail.com>
2020-08-22 14:01:23 -0400

    Added player sprite.

--- js/canvas.js
+++ js/canvas.js
@@ -1,11 +1,12 @@
 class Canvas {
     constructor(id) {
-        this.canvas         = document.getElementById(id)
-        this.context        = this.canvas.getContext('2d')
-        this.width          = this.canvas.width
-        this.height         = this.canvas.height
-        this.imgLoaded      = 0
-        this.spriteSheet    = new Image()
+        this.canvas     = document.getElementById(id)
+        this.context    = this.canvas.getContext('2d')
+        this.width      = this.canvas.width
+        this.height     = this.canvas.height
+        this.imgLoaded  = 0
+        this.bgSprites  = new Image()
+        this.playerSprites  = new Image()
 
         this.setBackground = function (color) {
             this.context.fillStyle = color
@@ -13,18 +14,20 @@ class Canvas {
         }
 
         this.loadImages = function () {
-            this.spriteSheet.src = "./assets/sprites-outlines.png"
+            this.bgSprites.src = "./assets/sprites-outlines.png"
+            this.imgLoaded++
+            this.playerSprites.src = "./assets/industrial.png"
             this.imgLoaded++
         }
 
         this.drawBrick = function (x, y) {
             //this.context.clearRect(50, 50, 16, 16);
 
-            this.context.drawImage(this.spriteSheet, 184, 352, 16, 16, x, y, 16, 16)
+            this.context.drawImage(this.bgSprites, 184, 352, 16, 16, x, y, 16, 16)
         }
 
         this.drawPlayer = function (player) {
-            this.context.drawImage(this.spriteSheet, 136, 256, 16, 16, player.getX(), player.getY(), 16, 16)
+            this.context.drawImage(this.playerSprites, 1, 258, 14, 14, player.getX(), player.getY(), 16, 16)
         }
     }
 }
